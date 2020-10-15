1866c4f5fa3605827b04ed297cc063e19a783b49
Ernest <web.ernest@outlook.com>
2020-10-15 03:10:34 +0000

    Changed how program handles user input

--- setup.cpp
+++ setup.cpp
@@ -5,94 +5,92 @@ using std::cout, std::cin, std::endl, std::string, std::ofstream;
 
 int main() {
 
-    string days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
     // open html file
-    ofstream outFS;
-    outFS.open("index.html");
+    ofstream ofs("index.html");
 
     // continue if file is valid
-    if (outFS.is_open()) {
+    if (ofs.is_open()) {
 
-        outFS << "<!DOCTYPE html>" << endl;
-        outFS << "<html lang=\"en\" dir=\"ltr\">" << endl;
-        outFS << "<head>" << endl;
-        outFS << "  <meta charset=\"utf-8\">" << endl;
-        outFS << "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" << endl;
-        outFS << "  <title>Timetable</title>" << endl;
-        outFS << "  <link rel=\"stylesheet\" href=\"stylesheets/style.css\">" << endl;
-        outFS << "  <link rel=\"stylesheet\" href=\"stylesheets/font.css\">" << endl;
-        outFS << "</head>" << endl;
-        outFS << "<body>" << endl;
-        outFS << "  <h1 id=\"title\">Timetable</h1>" << endl;
+        ofs << "<!DOCTYPE html>" << endl;
+        ofs << "<html lang=\"en\" dir=\"ltr\">" << endl;
+        ofs << "<head>" << endl;
+        ofs << "  <meta charset=\"utf-8\">" << endl;
+        ofs << "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" << endl;
+        ofs << "  <title>Timetable</title>" << endl;
+        ofs << "  <link rel=\"stylesheet\" href=\"stylesheets/style.css\">" << endl;
+        ofs << "  <link rel=\"stylesheet\" href=\"stylesheets/font.css\">" << endl;
+        ofs << "</head>" << endl;
+        ofs << "<body>" << endl;
+        ofs << "  <h1 id=\"title\">Timetable</h1>" << endl;
 
         unsigned classesInDay;
-        string className;
-        string startTime;
-        string endTime;
-        string meetingId;
-        string hasPassword;
-
-        cout << "Do not enter spaces." << endl;
+        string className, startTime, endTime, meetingId, hasPassword;
+        string days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
         // iterate for each day of the week
         for (size_t i = 0; i < 7; i++) {
+            
             cout << "Enter the number of meetings you have on " << days[i] << ": ";
-            cin >> classesInDay;
+			cin >> classesInDay;
+			
+			if (!cin.good()) {
+				cout << "You must enter a number." << endl;
+				return 1;
+			}
+			cin.ignore();
 
-            outFS << "  <div class=\"day\" id=\"d" << i << "\">" << endl;
+            ofs << "  <div class=\"day\" id=\"d" << i << "\">" << endl;
+            
+            // special <div> if there are no classes
             if (classesInDay == 0) {
-                outFS << "    <p>Nothing today!</p>" << endl;
+                ofs << "    <p>Nothing today!</p>" << endl;
             }
 
             // iterate for each class
             for (size_t j = 0; j < classesInDay; j++) {
-                outFS << "    <div class=\"event\">" << endl;
+				// <div> starting tag
+                ofs << "    <div class=\"event\">" << endl;
 
                 cout << "  Enter the name of class " << (j + 1) << ": ";
-                cin >> className;
+                getline(cin, className);
 
                 cout << "  Enter the time it starts (eg. 12:00): ";
-                cin >> startTime;
+                getline(cin, startTime);
 
                 cout << "  Enter the time it ends: ";
-                cin >> endTime;
+                getline(cin, endTime);
 
                 cout << "  Enter its meeting id: ";
-                cin >> meetingId;
+                getline(cin, meetingId);
 
-                outFS << "      <p>" << startTime << " - " << endTime << "<span>" << className << "</span></p>" << endl;
-                outFS << "      <p>" << meetingId << "</p>" << endl;
+                ofs << "      <p>" << startTime << " - " << endTime << "<span>" << className << "</span></p>" << endl;
+                ofs << "      <p>" << meetingId << "</p>" << endl;
 
                 cout << "  Does the meeting require a password? (y/n): ";
-                cin >> hasPassword;
+                getline(cin, hasPassword);
 
                 if (hasPassword == "y" || hasPassword == "yes") {
                     string password = "";
                     cout << "    Enter its password: ";
-                    cin >> password;
-                    outFS << "      <p>" << password << "</p>" << endl;
+                    getline(cin, password);
+                    ofs << "      <p>" << password << "</p>" << endl;
                 }
-                outFS << "    </div>" << endl;
+                ofs << "    </div>" << endl;
             }
 
-            if (classesInDay > 0) {
-                cout << endl;
-            }
-            outFS << "  </div>" << endl;
+            ofs << "  </div>" << endl;
         }
 
-        outFS << "  <script type=\"text/javascript\" src=\"scripts/day.js\"></script>" << endl;
-        outFS << "</body>" << endl;
-        outFS << "</html>" << endl;
+        ofs << "  <script type=\"text/javascript\" src=\"scripts/day.js\"></script>" << endl;
+        ofs << "</body>" << endl;
+        ofs << "</html>" << endl;
 
         cout << "Your configuration has been saved. Open index.html to see your timetable. You can also make changes there." << endl;
-        outFS.close();
 
     } else {
         cout << "Problem opening file." << endl;
+        return 1;
     }
 
     return 0;
 }
-
